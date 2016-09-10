8b10bf4f4e1bd31e8fddf677f184f6df7e8b6ee6
deniskovalenko <denis.v.kovalenko@gmail.com>
2016-09-10 14:16:05 +0300

    add facebook authentication

---
+++ app/src/main/java/com/atm/atm/AtmApplication.java
@@ -0,0 +1,28 @@
+package com.atm.atm;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import java.util.Date;
+
+/**
+ * Created by denis on 10.09.16.
+ */
+public class AtmApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+}
