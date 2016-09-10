be83c5221b17f2d41062a23ea19e4cfcaa894549
andreasvija <andreasvija@hotmail.com>
2016-09-10 21:14:37 +0300

    create add event activity

---
+++ app/src/main/java/com/atm/atm/EventCreateActivity.java
@@ -0,0 +1,42 @@
+package com.atm.atm;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class EventCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_create);
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+        LinearLayout rlayout = (LinearLayout) findViewById(R.id.cameralayout);
+        rlayout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //take pic
+                //crop
+                //upload
+                //return url
+            }
+
+        });
+    }
+
+}
